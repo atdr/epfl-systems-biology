b2d3832aaba33d7ee9a7ba8378a94c583324b50f
Andreas Richardson <atdr@users.noreply.github.com>
2019-10-10 15:06:21 +0200

    initial working solution for growth rate and shadow price of all substrates

--- exercise1/Exercises1_Part4_3_10102019.m
+++ exercise1/Exercises1_Part4_3_10102019.m
@@ -1,32 +1,51 @@
-clc
-clear all
-addpath(genpath('C:\AAA\EPFL Y3\Principles and Application of Systems biology\MATLAB\matTFA-master'));
-addpath(genpath('C:\Program Files\IBM\ILOG\CPLEX_Studio129\cplex\matlab'))
-changeCobraSolver('cplex_direct');
-load('C:\AAA\EPFL Y3\Principles and Application of Systems biology\MATLAB\ecoli_core_model.mat')
+clear, clc, close all
+load('ecoli_core_model.mat')
 
 charPts = [1,3,6,10,20];
+sol = struct;
 
-growthRates = zeros(21);
-for
-for i = 0:30
-for j = 0:30
-model = changeRxnBounds(model,'EX_glc(e)',-i,'b');
-model = changeRxnBounds(model,'EX_o2(e)',-j,'b');
-FBAsolution = optimizeCbModel(model,'max');
-growthRates(i+1,j+1) = FBAsolution.f;
-try
-   shadowPrices(i+1,j+1) = FBAsolution.w(28);
-catch
-   shadowPrices(i+1,j+1) = NaN;
+% flux limit in each dimension
+upperFlux = 30;
+
+% list of substrates to consider
+subs = {'EX_glc(e)'; 'EX_pyr(e)'; 'EX_succ(e)'};
+
+for subIndex = 1:numel(subs)
+    sub = subs(subIndex);
+    sol(subIndex).label = sub;
+    sol(subIndex).growthRates = zeros(upperFlux);
+    sol(subIndex).shadowPrices = zeros(upperFlux);
+
+% substrate flux
+for subFlux = 0:upperFlux
+    model = changeRxnBounds(model,sub,-subFlux,'b');
+    
+    % oxygen flux
+    for oxFlux = 0:upperFlux
+        model = changeRxnBounds(model,'EX_o2(e)',-oxFlux,'b');
+        
+        % do the optimistion
+        FBAsolution = optimizeCbModel(model,'max');
+        
+        % save the growth rate
+        sol(subIndex).growthRates(subFlux+1,oxFlux+1) = FBAsolution.f;
+        
+        % save the shadow price (catch errors)
+        try
+            sol(subIndex).shadowPrices(subFlux+1,oxFlux+1) = FBAsolution.w(28);
+        catch
+            sol(subIndex).shadowPrices(subFlux+1,oxFlux+1) = NaN;
+        end
+        
+    end
 end
 
-if (i == 10 & ismember(j,charPts))
-    writetable(table(model.rxns,FBAsolution.x), ['ptype_glucose_10_' num2str(j) '_flux_data.csv']);
 end
-end
-end
-end
+
+% if (i == 10 & ismember(j,charPts))
+%     writetable(table(model.rxns,FBAsolution.x), ['ptype_glucose_10_' num2str(j) '_flux_data.csv']);
+% end
+
 
 figure (3)
 pcolor([0:1:30],[0:1:30], shadowPrices)
